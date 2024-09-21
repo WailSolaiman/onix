93c2cccb421b4f4490fb2b9f47465cbbcc36beed
Wail Solaiman <wailsolaiman@hotmail.com>
2024-09-21 04:31:18 +0200

    edit navbar component, install framer-motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Insights from './components/Insights'
+import Stats from './components/Stats'
+import Pricing from './components/Pricing'
+import Faq from './components/Faq'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
 function App() {
-	return <div className='text-3xl text-center mt-8'>SaaS Landing Page</div>
+	return (
+		<div className='bg-gradient-to-r from-indigo-50 via-purple-50 to-pink-50'>
+			<Navbar />
+			<Hero />
+			<Insights />
+			<Stats />
+			<Pricing />
+			<Testimonials />
+			<Faq />
+			<Footer />
+		</div>
+	)
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,66 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+
+const fadeIn = {
+	initial: { opacity: 0 },
+	animate: { opacity: 1 },
+	transition: { delay: 0.3, staggerChildren: 0.1 },
+}
+
+const hoverTapEffect = {
+	whileHover: {
+		textShadow: '0 0 8px rgba(0, 200, 255, 0.8)', // Adds a glow effect on hover
+		color: '#00bfff', // Changes text color on hover
+	},
+	whileTap: {
+		textShadow: 'none', // Removes glow on tap
+		color: '#1f2937', // Resets to original color
+	},
+}
+
+const buttonHoverEffect = {
+	whileHover: {
+		textShadow: '0 0 12px rgba(0, 200, 255, 0.8)', // More pronounced glow on hover
+		color: '#ffffff', // Change button text color
+	},
+	whileTap: {
+		textShadow: 'none', // Remove glow on tap
+		color: '#1f2937', // Reset color on tap
+	},
+}
+
+// Reusable Animated List Item component
+const AnimatedLi = ({ children, href }) => (
+	<motion.li {...hoverTapEffect}>
+		<a href={href}>{children}</a>
+	</motion.li>
+)
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	const menuItems = ['Portfolio', 'Our Team', 'Careers', 'About']
+
+	return (
+		<div className='w-4/5 m-auto flex justify-between items-center py-5'>
+			<h1 className='text-xl font-bold'>BetaTech</h1>
+			<motion.ul
+				{...fadeIn}
+				className='hidden md:flex items-center space-x-5'>
+				{menuItems.map((item, index) => (
+					<AnimatedLi
+						key={index}
+						href={`#${item.toLowerCase().replace(/\s+/g, '-')}`}>
+						{item}
+					</AnimatedLi>
+				))}
+			</motion.ul>
+			<motion.button
+				{...buttonHoverEffect}
+				className='px-5 py-2 border shadow-md text-black 
+				hover:bg-gradient-to-r hover:from-teal-400 hover:to-blue-500 rounded-full'>
+				Register
+			</motion.button>
+		</div>
+	)
 }
 
 export default Navbar
